7a6bdbbffb8f270b70110a4baa1de43af0df357e
unknownpgr <unknownpgr@gmail.com>
2020-11-08 01:40:18 +0900

    Refactor

--- main.cpp
+++ main.cpp
@@ -9,9 +9,24 @@ using namespace cv;
 
 #define LOG(msg) printf(msg "\n");
 
+// Motor control
 #define PWM_MAX 128
-#define PUNCH_THRESH 0.5
+#define PUNCH_THRESH 0.5f
 #define PUNCH_T 2
+
+// Image processing
+#define IMAGE_DOWNSIZE 4
+#define IMAGE_ROI 2 / 3
+#define COLOR_MEAN 30
+#define COLOR_RANGE 5
+
+// Drive control
+#define POS_GAIN 3.5f
+#define VELO_MAIN 0.7f
+
+// Log / Debugging
+#define DEBUG_CAPTURE 0
+#define DEBUG_LOG 1
 
 int pins[4] = {PIN_L_A,
                PIN_L_B,
@@ -29,31 +44,39 @@ void init()
 }
 
 // Set velocity of one wheel
-void setVeloWheel(int pinGND, int pinSGN, float value)
+void setVeloWheel(int pinBASE, int pinSGN, float value)
 {
   // If value is minus, swap pin and use abs(value)
   if (value < 0)
   {
-    int tmp = pinGND;
-    pinGND = pinSGN;
+    int tmp = pinBASE;
+    pinBASE = pinSGN;
     pinSGN = tmp;
     value = -value;
   }
 
-  softPwmWrite(pinGND, PWM_MAX);
+  // Set base pin to 1, because L9110s motordriver uses pull-up.
+  softPwmWrite(pinBASE, PWM_MAX);
+
   // If value is too small, punch.
   if (value < PUNCH_THRESH)
   {
     softPwmWrite(pinSGN, 0);
     delay(PUNCH_T);
   }
-  if (value > 1)
-    value = 1;
+
+  // Calculate pwm (we inverse the value because the signal is 0, not 1.)
   int pwm = (int)(PWM_MAX * (1 - value));
+  if (pwm > PWM_MAX)
+  {
+    pwm = PWM_MAX;
+  }
+
+  // Apply pwm
   softPwmWrite(pinSGN, pwm);
 }
 
-// Set velocity of each wheel
+// Set velocity of both wheels
 void setVelo(float left, float right)
 {
   setVeloWheel(PIN_L_A, PIN_L_B, left);
@@ -72,20 +95,22 @@ int main(void)
   LOG("Open video");
   VideoCapture cap(0);
 
-  const int center = 30;
-  const int delta = 5;
-  const float gain = 3.5;
+  // Variables for image processing
   Mat org, hsv, mask, hsvSplit[3];
-
   cap.read(org);
-
   Size sizeOrg = org.size();
-  Size sizeSmall = Size(sizeOrg.width / 4, sizeOrg.height / 4);
+  Size sizeSmall = Size(sizeOrg.width / IMAGE_DOWNSIZE, sizeOrg.height / IMAGE_DOWNSIZE);
+
+  // Variables for drive control
   int i, y, x, weightSum, positionSum;
   float position, left, right, posBef;
+
+  // Variables for debugging
   char posBar[21];
   posBar[20] = 0;
   posBef = 0;
+
+  LOG("Start linetracing");
   for (;;)
   {
     // Read image from camera
@@ -98,16 +123,17 @@ int main(void)
     split(hsv, hsvSplit);
 
     // Get lane mask
-    bitwise_and((center - delta) < hsvSplit[0], hsvSplit[0] < (center + delta), mask);
-#if 0
+    bitwise_and((COLOR_MEAN - COLOR_RANGE) < hsvSplit[0], hsvSplit[0] < (COLOR_MEAN + COLOR_RANGE), mask);
+
+#if DEBUG_CAPTURE
     imwrite("test.jpg", mask);
     return 0;
 #endif
 
-    // Calculate lane position
+    // Calculate lane position (weighted sum of lane pixels)
     weightSum = 0;
     positionSum = 0;
-    for (y = sizeSmall.height * 2 / 3; y < sizeSmall.height; y++)
+    for (y = sizeSmall.height * IMAGE_ROI; y < sizeSmall.height; y++)
     {
       uchar *row = mask.ptr<uchar>(y);
       for (x = 0; x < sizeSmall.width; x++)
@@ -116,25 +142,23 @@ int main(void)
         positionSum += row[x] * x;
       }
     }
+    // Update position only if there are significantly many lane pixels.
     if (weightSum > 50)
     {
-      position = positionSum * 1.f / weightSum;
-      position /= sizeSmall.width; // Normalize
-      position -= 0.5f;            // Remove bias
-      position *= gain;            // Gain
-      if (position > 0.98f)
-        position = 0.98f;
-      if (position < -0.98f)
-        position = -0.98f;
+      position = positionSum * 1.f / weightSum; // Calculated weighted mean
+      position /= sizeSmall.width;              // Normalize
+      position -= 0.5f;                         // Remove bias
+      position *= POS_GAIN;                     // Apply gain
     }
 
-    // posBef = position * 0.5f + posBef * 0.5f;
-    // position = posBef;
+    // Calculated wheel velocity from position
+    left = VELO_MAIN * (1 + position);
+    right = VELO_MAIN * (1 - position);
 
-    left = 0.7f * (1 + position);
-    right = 0.7f * (1 - position);
+    // Apply wheel velocity
+    setVelo(left, right);
 
-#if 1
+#if DEBUG_LOG
     int intPos = (position + 1) * 10;
     if (intPos < 0)
       intPos = 0;
@@ -151,8 +175,6 @@ int main(void)
     }
     printf("%s : %+2.2f\t%+2.2f\n", posBar, left, right);
 #endif
-
-    setVelo(left, right);
   }
   return 0;
 }
