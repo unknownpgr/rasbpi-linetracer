bcc979b22fba738e8ad29d8f70c079f89e94bda8
unknownpgr <unknownpgr@gmail.com>
2020-11-07 22:22:06 +0900

    Implement PWM control

--- config.h
+++ config.h
@@ -1,4 +1,4 @@
 #define PIN_L_A 7
 #define PIN_L_B 0
-#define PIN_R_A 2
-#define PIN_R_B 3
+#define PIN_R_A 3
+#define PIN_R_B 2
--- main.c
+++ main.c
@@ -1,39 +1,78 @@
 #include "config.h"
+#include <math.h>
+#include <softPwm.h>
 #include <stdio.h>
 #include <wiringPi.h>
 
+#define LOG(msg) printf(msg "\n");
+
+#define PWM_MAX 128
+#define PUNCH_THRESH 0.5
+#define PUNCH_T 2
+
+int pins[4] = {PIN_L_A,
+               PIN_L_B,
+               PIN_R_A,
+               PIN_R_B};
+
+void init()
+{
+  // Set all pins to high
+  for (int i = 0; i < 4; i++)
+  {
+    pinMode(pins[i], OUTPUT);
+    softPwmCreate(pins[i], PWM_MAX, PWM_MAX);
+  }
+}
+
+// Set velocity of one wheel
+void setVeloWheel(int pinGND, int pinSGN, float value)
+{
+  // If value is minus, swap pin and use abs(value)
+  if (value < 0)
+  {
+    int tmp = pinGND;
+    pinGND = pinSGN;
+    pinSGN = tmp;
+    value = -value;
+  }
+
+  softPwmWrite(pinGND, PWM_MAX);
+  // If value is too small, punch.
+  if (value < PUNCH_THRESH)
+  {
+    softPwmWrite(pinSGN, 0);
+    delay(PUNCH_T);
+  }
+  softPwmWrite(pinSGN, (int)(PWM_MAX * (1 - value)));
+}
+
+// Set velocity of each wheel
+void setVelo(float left, float right)
+{
+  setVeloWheel(PIN_L_A, PIN_L_B, left);
+  setVeloWheel(PIN_R_A, PIN_R_B, right);
+}
+
 int main(void)
 {
-  printf("Raspberry Pi blink\n");
-
+  LOG("Check wiringPi setup");
   if (wiringPiSetup() == -1)
     return 1;
 
-  int pins[4] = {
-      PIN_L_A,
-      PIN_L_B,
-      PIN_R_A,
-      PIN_R_B};
+  LOG("Initialize software pwm");
+  init();
 
-  for (int i = 0; i < 4; i++)
-    pinMode(pins[i], OUTPUT);
-
-  for (int i = 0;; i++)
+  LOG("Wheel speed test");
+  for (;;)
   {
-    i %= 4;
-    printf("Activate : pins[%d]=%d\n", i, pins[i]);
-    for (int j = 0; j < 4; j++)
+    for (float t = 0; t < 3.1415926535897932384626 * 2; t += 0.01)
     {
-      if (i == j)
-      {
-        digitalWrite(pins[j], 0);
-      }
-      else
-      {
-        digitalWrite(pins[j], 1);
-      }
+      float left = sin(t);
+      float right = cos(t);
+      setVelo(left, right);
+      delay(10);
     }
-    delay(1000);
   }
   return 0;
 }
